38631101bd59a0b9807cbb401a6f7fe2ef0bd4ee
jfsenechal <jfsenechal@gmail.com>
2025-07-15 14:05:41 +0200

    up

--- Lib/Menu.php
+++ Lib/Menu.php
@@ -44,6 +44,7 @@ class Menu
                     continue;
                 }
                 $row->slug = $post->post_name;
+                $row->type = 'article';
                 $row->parents = $this->wpRepository->getAncestorsOfPost((int)$row->object_id);
             }
             if ($row->object === 'page') {
@@ -52,12 +53,14 @@ class Menu
                     continue;
                 }
                 $row->slug = $page->post_name;
+                $row->type = 'article';
                 $row->parents = [];
             }
             if ($row->object === 'category') {
                 $category = get_category($row->object_id);
                 if ($category) {
                     $row->slug = $category->slug;
+                    $row->type = 'category';
                     $row->parents = $this->wpRepository->getAncestorsOfCategory((int)$row->object_id);
                 }
             }
